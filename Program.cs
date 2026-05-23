d1c34324f9e3f8ffa2bc4f339177a87eb175602c
P1N60 <victor.guldregn@gmail.com>
2026-05-23 12:05:04 +0200

    1.c

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
 
 namespace Hashing
 {
@@ -6,19 +8,55 @@ namespace Hashing
     {
         static void Main(string[] args)
         {
-            ulong x = 123456789UL;
-            int l = 20;
+            TestHashingPerformance(1000000, 16);
+            TestHashingPerformance(10000000, 20);
+        }
 
-            ulong msA = 13845678901234567891UL;
-            ulong msResult = Opgave1.MultiplyShift(x, msA, l);
-            Console.WriteLine($"MultiplyShift: h({x}) = {msResult}");
+        static void TestHashingPerformance(int n, int l)
+        {
+            Console.WriteLine($"\nRunning test with n = {n}, l = {l} (Number of distinct keys = 2^{l})");
 
+            // Generate random parameters for Multiply-Shift
+            Random rnd = new Random();
+            byte[] aShiftBytes = new byte[8];
+            rnd.NextBytes(aShiftBytes);
+            ulong aShift = BitConverter.ToUInt64(aShiftBytes, 0) | 1UL; // Ensure it's odd
+
+            // Generate random parameters for Multiply-Mod-Prime
+            byte[] aPrimeBytes = new byte[12]; // ~96 bits, will mask down to 89
+            byte[] bPrimeBytes = new byte[12];
+            rnd.NextBytes(aPrimeBytes);
+            rnd.NextBytes(bPrimeBytes);
+            
             BigInteger p = (BigInteger.One << 89) - 1;
-            BigInteger mmpA = p - 1;
-            BigInteger mmpB = 42;
-            ulong mmpResult = Opgave1.MultiplyModPrime(x, mmpA, mmpB, l);
-            Console.WriteLine($"MultiplyModPrime: h({x}) = {mmpResult}");
-            Console.WriteLine($"Burde være 275220");
+            BigInteger aPrime = new BigInteger(aPrimeBytes, isUnsigned: true) % p;
+            BigInteger bPrime = new BigInteger(bPrimeBytes, isUnsigned: true) % p;
+            
+            // Generate stream
+            var stream = StreamGenerator.CreateStream(n, l);
+
+            // Test Multiply-Shift
+            ulong sumShift = 0;
+            Stopwatch swShift = Stopwatch.StartNew();
+            foreach (var item in stream)
+            {
+                sumShift += Opgave1.MultiplyShift(item.Item1, aShift, l);
+            }
+            swShift.Stop();
+            Console.WriteLine($"Multiply-Shift: Time = {swShift.ElapsedMilliseconds} ms, Sum = {sumShift}");
+
+            // Regenerate stream
+            stream = StreamGenerator.CreateStream(n, l);
+
+            // Test Multiply-Mod-Prime
+            ulong sumPrime = 0;
+            Stopwatch swPrime = Stopwatch.StartNew();
+            foreach (var item in stream)
+            {
+                sumPrime += Opgave1.MultiplyModPrime(item.Item1, aPrime, bPrime, l);
+            }
+            swPrime.Stop();
+            Console.WriteLine($"Multiply-Mod-Prime: Time = {swPrime.ElapsedMilliseconds} ms, Sum = {sumPrime}");
         }
     }
 }
